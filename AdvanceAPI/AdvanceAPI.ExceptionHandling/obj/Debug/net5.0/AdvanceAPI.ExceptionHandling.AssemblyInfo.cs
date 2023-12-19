8afec5a88087e3364a2c862ef3e02774c4efd66c
furkangokirmak <44866183+furkangokirmak@users.noreply.github.com>
2023-12-19 16:15:31 +0300

    database updated

--- AdvanceAPI/AdvanceAPI.ExceptionHandling/obj/Debug/net5.0/AdvanceAPI.ExceptionHandling.AssemblyInfo.cs
+++ AdvanceAPI/AdvanceAPI.ExceptionHandling/obj/Debug/net5.0/AdvanceAPI.ExceptionHandling.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvanceAPI.ExceptionHandling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223065d72c4946501414e3a2f7f83937bb33f17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvanceAPI.ExceptionHandling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvanceAPI.ExceptionHandling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
